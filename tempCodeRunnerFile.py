18a694976e9cfb7c6ab09e197de6d8814d753f87
stellaribli <riblistella@gmail.com>
2021-11-23 08:25:53 +0700

    arrayy

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+for i in range len(arraykeywords):
+#     textkeywords = textkeywords.arraykeywords[i]
--- template.py
+++ template.py
@@ -2,24 +2,35 @@ from flask import Flask, render_template, request, request, redirect, url_for
 from werkzeug.utils import secure_filename 
 import os
 
-
 app = Flask(__name__)
-x=12
 @app.route('/')
 @app.route('/index')
 def index():
     name = 'StelalCantik'
     nilai = 122
-    textkeywords = 'Saran gue sich lo mending pake keyword a, b, c ye'
-    textsaran = 'Iuh jelek CVnya'
+
+    # Untuk Keywords bagian tengah 
+    textkeywords = ''
+    arraykeywords = ['aa','b']
+    for i in range (len(arraykeywords)):
+        textkeywords = textkeywords + ' ' + (arraykeywords[i])
+
+    # Untuk Tips bagian kanan
+    textsaran = ''
+    arrayksaran = ['aaasdfa','bdfafad']
+    for i in range (len(arrayksaran)):
+        textsaran = textsaran + ' ' + (arrayksaran[i])
+
     return render_template('index.html', username=name, keywords = textkeywords, saran = textsaran, cvscore = nilai)
 
+namafilecv = ""
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
         uploaded_file = request.files['file']
         if uploaded_file.filename != '':
             uploaded_file.save(uploaded_file.filename)
+        namafilecv = uploaded_file.filename
         return redirect(url_for('index'))
     return render_template('upload.html')
 
