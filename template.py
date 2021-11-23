0ac1fe315ab39c42e1aa1b9ea96313607f56e931
stellaribli <riblistella@gmail.com>
2021-11-23 09:23:45 +0700

    BISAPILIHH

--- template.py
+++ template.py
@@ -4,14 +4,20 @@ import os
 
 app = Flask(__name__)
 @app.route('/')
-@app.route('/index')
-def index():
-    name = 'StelalCantik'
+
+
+@app.route('/pilih')
+def pilih():
+    return render_template('pilih.html')
+
+@app.route('/PM')
+def pm():
+    name = 'Calon PM'
     nilai = 122
 
     # Untuk Keywords bagian tengah 
     textkeywords = ''
-    arraykeywords = ['aa','b']
+    arraykeywords = ['Kalimat satu','Kalimat lalalala b']
     for i in range (len(arraykeywords)):
         textkeywords = textkeywords + ' ' + (arraykeywords[i])
 
@@ -23,6 +29,72 @@ def index():
 
     return render_template('index.html', username=name, keywords = textkeywords, saran = textsaran, cvscore = nilai)
 
+
+
+@app.route('/SE')
+def se():
+    name = 'Calon SE'
+    nilai = 122
+
+    # Untuk Keywords bagian tengah 
+    textkeywords = ''
+    arraykeywords = ['Kalimat satu','Kalimat lalalala b']
+    for i in range (len(arraykeywords)):
+        textkeywords = textkeywords + ' ' + (arraykeywords[i])
+
+    # Untuk Tips bagian kanan
+    textsaran = ''
+    arrayksaran = ['aaasdfa','bdfafad']
+    for i in range (len(arrayksaran)):
+        textsaran = textsaran + ' ' + (arrayksaran[i])
+
+    return render_template('index.html', username=name, keywords = textkeywords, saran = textsaran, cvscore = nilai)
+
+
+
+@app.route('/Finance')
+def fin():
+    name = 'Calon Finance bitj'
+    nilai = 122
+
+    # Untuk Keywords bagian tengah 
+    textkeywords = ''
+    arraykeywords = ['Kalimat satu','Kalimat lalalala b']
+    for i in range (len(arraykeywords)):
+        textkeywords = textkeywords + ' ' + (arraykeywords[i])
+
+    # Untuk Tips bagian kanan
+    textsaran = ''
+    arrayksaran = ['aaasdfa','bdfafad']
+    for i in range (len(arrayksaran)):
+        textsaran = textsaran + ' ' + (arrayksaran[i])
+
+    return render_template('index.html', username=name, keywords = textkeywords, saran = textsaran, cvscore = nilai)
+
+
+
+@app.route('/Designer')
+def des():
+    name = 'Calon Designer apalah'
+    nilai = 122
+
+    # Untuk Keywords bagian tengah 
+    textkeywords = ''
+    arraykeywords = ['Kalimat satu','Kalimat lalalala b']
+    for i in range (len(arraykeywords)):
+        textkeywords = textkeywords + ' ' + (arraykeywords[i])
+
+    # Untuk Tips bagian kanan
+    textsaran = ''
+    arrayksaran = ['aaasdfa','bdfafad']
+    for i in range (len(arrayksaran)):
+        textsaran = textsaran + ' ' + (arrayksaran[i])
+
+    return render_template('index.html', username=name, keywords = textkeywords, saran = textsaran, cvscore = nilai)
+
+
+
+
 namafilecv = ""
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
@@ -33,5 +105,6 @@ def upload():
         namafilecv = uploaded_file.filename
         return redirect(url_for('index'))
     return render_template('upload.html')
+
 
 app.run(host='0.0.0.0', port=80)
