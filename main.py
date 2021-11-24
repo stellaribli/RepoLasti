f18430dbc7e1b8c13d0f57bfa71750a5db1ea47e
stellaribli <riblistella@gmail.com>
2021-11-25 03:30:39 +0700

    tesseract fix

--- main.py
+++ main.py
@@ -5,13 +5,7 @@ import pytesseract
 import cv2
 from waitress import serve
 pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
-# ON_HEROKU = os.environ.get('ON_HEROKU')
-# heroku=''
-# if ON_HEROKU:
-#     # get the heroku port
-#     port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
-# else:
-#     port = 3000
+
 
 def create_app():
     return app
